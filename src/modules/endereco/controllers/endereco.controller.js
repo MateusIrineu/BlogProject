b261c7e3716aa3adbf778af51235b531078abb88
Mateus Irineu <irineu.mateus11@gmail.com>
2025-08-05 21:12:07 -0300

    endereco

--- src/modules/endereco/controllers/endereco.controller.js
+++ src/modules/endereco/controllers/endereco.controller.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import EnderecoModel from "../models/endereco.model";
 
 class EnderecoController {
@@ -8,6 +9,27 @@ class EnderecoController {
             if (!user_id || !cep || !numero) {
                 return res.status(400).json({ message: 'Erro do cliente.' })
             }
+
+            let dados = {}
+            if (cep) {
+                 const resposta = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+                    if (resposta.data.erro) {
+                    return res.status(400).json({ message: 'CEP inválido.' });
+                }
+                
+                dados = {
+                    cep,
+                    rua: resposta.data.logradouro,
+                    estado: resposta.data.uf,
+                    cidade: resposta.data.localidade,
+                    bairro: resposta.data.bairro
+                }
+            }
+             
+            if (numero) {
+                dados.numero = numero
+            }
+
 
             const endereco = await EnderecoModel.create({ user_id, cep, numero });
             res.status(201).json({ message: 'Endereço criado com sucesso!', endereco: endereco })
